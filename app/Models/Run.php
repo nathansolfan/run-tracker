23a7a37bb864343528aaee836209003ec68408f8
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-03-29 00:00:30 +0000

    updated model

--- app/Models/Run.php
+++ app/Models/Run.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Run extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'distance',
+        'duration',
+        'pace',
+        'average_speed',
+        'started_at',
+        'ended_at',
+        'route_data',
+        'elevation_gain',
+        'weather_conditions',
+        'notes',
+    ];
+
+
 }
