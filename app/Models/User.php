b017ec6205e6e3cadc6c0aed042b8420c41de33b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-03-29 16:48:40 +0000

    update hasMany()

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,14 @@ class User extends Authenticatable
     {
         return Str::of($this->name)
             ->explode(' ')
-            ->map(fn (string $name) => Str::of($name)->substr(0, 1))
+            ->map(fn(string $name) => Str::of($name)->substr(0, 1))
             ->implode('');
+    }
+
+    /**
+     * Get the runs for the user.
+     */
+    public function runs() {
+        return $this->hasMany(Run::class);
     }
 }
