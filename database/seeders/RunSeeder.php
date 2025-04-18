c68abc75870cdb0bfb8c76f58c41787717b23f43
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-04-18 13:58:06 +0100

    updates

--- database/seeders/RunSeeder.php
+++ database/seeders/RunSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Run;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,16 @@ class RunSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // create the first test user if noone exist
+        $user = User::first() ?? User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        // create 15 runs for the user
+        Run::factory()
+        ->count(15)
+        ->for($user)
+        ->create();
     }
 }
