feb8529c5b4ef759c7b80321a9145dfc199aca63
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-04-24 23:34:52 +0100

    update

--- resources/views/runs/track.blade.php
+++ resources/views/runs/track.blade.php
@@ -31,10 +31,10 @@
                             <button id="stop-tracking" class="px-6 py-2 bg-red-600 text-white rounded-md" disabled>End Run</button>
                         </div>
 
-                        {{-- <div class="bg-blue-50 p-6 rounded-lg mb-6">
+                        <div class="bg-blue-50 p-6 rounded-lg mb-6">
                             <h3 class="text-lg font-medium text-gray-900 mb-4">Route Map</h3>
                             <div id="route-map" class="h-124 w-full rounded-lg"></div>
-                             --}}
+                            
                             <div class="mt-4">
                                 <label class="block font-medium text-sm text-gray-700 mb-2">Route Type</label>
                                 <select id="route-type" class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
