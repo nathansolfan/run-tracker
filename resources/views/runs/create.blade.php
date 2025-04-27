9ffa25db0be5f56228fa47bc1e944fa0f6840fda
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-04-27 23:37:14 +0100

    update

--- resources/views/runs/create.blade.php
+++ resources/views/runs/create.blade.php
@@ -32,14 +32,14 @@
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
                         </div>
-                        
+{{--                         
                         <div class="mb-4">
                             <label for="notes" class="block font-medium text-sm text-gray-700">{{ __('Notes (optional)') }}</label>
                             <textarea id="notes" name="notes" class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ old('notes') }}</textarea>
                             @error('notes')
                                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                             @enderror
-                        </div>
+                        </div> --}}
                         
                         <div class="flex items-center justify-end mt-4">
                             <a href="{{ route('runs.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-300 border border-transparent rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest hover:bg-gray-400 focus:bg-gray-400 active:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150 mr-2">
